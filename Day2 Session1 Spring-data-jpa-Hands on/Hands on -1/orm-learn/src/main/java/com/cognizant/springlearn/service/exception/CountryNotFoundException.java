9247102e47f58cc698cc9a217e93eedfc64e57c1
Subbarayudu <40607314+devtwin@users.noreply.github.com>
2021-03-26 18:52:39 +0530

    04 02-Spring hands-on

---
+++ Day2 Session1 Spring-data-jpa-Hands on/Hands on -1/orm-learn/src/main/java/com/cognizant/springlearn/service/exception/CountryNotFoundException.java
@@ -0,0 +1,9 @@
+package com.cognizant.springlearn.service.exception;
+
+public class CountryNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+}
